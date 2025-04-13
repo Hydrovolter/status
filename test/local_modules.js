3492ec50b76a26ba4d921e1a9f8c3ea36bcfb78e
Hydro <103881740+Hydrovolter@users.noreply.github.com>
2025-04-13 16:42:06 +0100

    add .env.example, local_modules

--- test/local_modules.js
+++ test/local_modules.js
@@ -1,2 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
+// for api/app.js AND bot/bot.js
+// import these modules if working locally
+
+// also run the following to install the module
+
+// npm install dotenv
